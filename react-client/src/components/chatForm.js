cdb70b6b338b2fd2145b536c80b3d0ddfb4d663f
safarudinyoga <safarudinyoga@gmail.com>
2019-10-27 01:03:55 +0700

    Adding HandleKey for textArea

--- react-client/src/components/chatForm.js
+++ react-client/src/components/chatForm.js
@@ -12,6 +12,7 @@ export default class ChatForm extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleKey = this.handleKey.bind(this);
     }
 
     handleChange(event) {
@@ -29,18 +30,27 @@ export default class ChatForm extends React.Component {
         })
     }
 
+    handleKey(event) {
+        if(event.keyCode === 13 && !event.shiftKey){
+            event.preventDefault()
+            let button = document.getElementById('buttonsubmit');
+            button.click();
+        }
+    }
+
+
     render() {
         return (
             <form className="form" onSubmit={this.handleSubmit}>
                 <li className="list-group-item borderless d-flex justify-content-between align-items-center">
-                    <button className="btn btn-light btn-circle btn-circle-lg m-1" type="submit"><i
+                    <button id="buttonsubmit" className="btn btn-light btn-circle btn-circle-lg m-1" type="submit"><i
                         className="fa fa-plus fa-2x"></i></button>
                     <div className="speech-bubble col-11">
                         <div className="form-label-group">
-                            <input type="text" name="name" className="form-control" placeholder="Name" value={this.state.name} autoFocus={true} onChange={this.handleChange} required={true} />
+                            <input type="text" name="name" className="form-control" placeholder="Name" value={this.state.name} autoFocus={true} onChange={this.handleChange} onKeyUp={this.handleKey} required={true} />
                         </div>
                         <div className="form-label-group my-0">
-                            <textarea name="message" className="form-control" placeholder="Type your message here ..." value={this.state.message} autoFocus={true} onChange={this.handleChange} required={true} />
+                            <textarea name="message" className="form-control" placeholder="Type your message here ..." value={this.state.message} autoFocus={true} onKeyUp={this.handleKey} onChange={this.handleChange} required={true} />
                         </div>
                     </div>
                 </li>
