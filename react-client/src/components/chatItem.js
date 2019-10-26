d30e07cd79bfdf3bbada6494f6221fda41d46321
safarudinyoga <safarudinyoga@gmail.com>
2019-10-26 20:44:53 +0700

    Resend

--- react-client/src/components/chatItem.js
+++ react-client/src/components/chatItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Item from './item'
+import Item from './item';
 
 function ChatItem(props) {
     let butcolor = ['btn-info', 'btn-success', 'btn-warning', 'btn-danger', 'btn-dark','btn-light']
     const list = props.data.map((item, index) =>
-        <Item key={index} id={item.id} name={item.name} status={item.status} message={item.message} color={butcolor[index % 6]} sent={item.sent} removeItem={() => props.deleteTodo(index)} /> //{...item} buat mecah item langsung
-    )
+        <Item key={index} id={item.id} name={item.name} status={item.status} message={item.message} color={butcolor[index % 6]} sent={item.sent} removeItem={() => props.deleteTodo(index)} resendItem={() => props.resendTodo(item.id, item.name, item.message)}/>
+        )//{...item} buat mecah item langsung
 
     return (
         <div>
