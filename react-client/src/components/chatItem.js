c3abee897124e1bbeb6654eceef2afb473c02e7d
safarudinyoga <safarudinyoga@gmail.com>
2019-10-26 21:36:29 +0700

    chat delete success

--- react-client/src/components/chatItem.js
+++ react-client/src/components/chatItem.js
@@ -4,7 +4,7 @@ import Item from './item';
 function ChatItem(props) {
     let butcolor = ['btn-info', 'btn-success', 'btn-warning', 'btn-danger', 'btn-dark','btn-light']
     const list = props.data.map((item, index) =>
-        <Item key={index} id={item.id} name={item.name} status={item.status} message={item.message} color={butcolor[index % 6]} sent={item.sent} removeItem={() => props.deleteTodo(index)} resendItem={() => props.resendTodo(item.id, item.name, item.message)}/>
+        <Item key={index} id={item.id} name={item.name} status={item.status} message={item.message} color={butcolor[index % 6]} sent={item.sent} removeItem={() => props.deleteTodo(item.id)} resendItem={() => props.resendTodo(item.id, item.name, item.message)}/>
         )//{...item} buat mecah item langsung
 
     return (
